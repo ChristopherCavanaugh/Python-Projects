4420ace5982e37559d63e2f8298fbf295cda2f7b
ChristopherCavanaugh <85709155+ChristopherCavanaugh@users.noreply.github.com>
2021-08-02 13:39:30 -0700

    Add files via upload

---
+++ class_inheritance.py
@@ -0,0 +1,17 @@
+class California: #This is the parent class
+    name = 'No name provided'
+    county = 'Marin'
+    town = 'San Anselmo'
+    county_code = 1234
+
+
+class Voting(California): #first Child class as "voting"
+    registered_to_vote = 'yes' #attributes of first child class
+    age = 27
+
+class RV(California): #second child class as "Reg voters"
+    mailing_address = ' ' #attributes of first child class
+    mailing_list =True
+    
+    
+    
